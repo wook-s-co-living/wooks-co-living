5ee0763fcc35911126621a7806d0a495197cd4b7
codesloth <djqmflsk@naver.com>
2023-05-25 16:07:17 +0900

    ADD market view

--- markets/urls.py
+++ markets/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 app_name="markets"
 urlpatterns = [
-    path('', views.market_index_view, name='market_index'),
-    # path('<int:market_pk>/', views.market_detail_view, name='market_detail'),
-    # path('create/', views.create_market_view, name='create_market'),
-    # path('<int:market_pk>/delete/', views.delete_market_view, name='delete_market'),
-    # path('<int:market_pk>/update/', views.update_market_view, name='update_market'),
-    # path('<int:market_pk>/likes/', views.market_likes_view, name='market_likes'),
+    path('', views.index, name='index'),
+    path('<int:market_pk>/', views.detail, name='detail'),
+    path('create/', views.create, name='create'),
+    path('<int:market_pk>/update/', views.update, name='update'),
+    path('<int:market_pk>/delete/', views.delete, name='delete'),
+    path('<int:market_pk>/likes/', views.likes, name='likes'),
 ]
