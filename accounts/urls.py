ee32a87ab380f5b6f3f8b406a6ab91f8054fdb25
sodamjeong <sodam828@naver.com>
2023-05-25 15:40:49 +0900

    CREATE login signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,11 @@ from . import views
 app_name="accounts"
 urlpatterns = [
     path("login/", views.login, name="login"),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
+    # path('update/', views.update, name='update'),
+    # path('password/', views.change_password, name='change_password'),
+    # path('delete/', views.delete, name='delete'),
+    # path('profile/<str:username>/', views.profile, name='profile'),
+    # path('<int:user_pk>/follow/', views.follow, name='follow'),
 ]
