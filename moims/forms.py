7066fa32adf4e14dcd5bde42932d13d6b317bfb2
“SSONGNOSS66” <ssongnoss66@gmail.com>
2023-05-28 02:39:50 +0900

    CREATE moims BE

--- moims/forms.py
+++ moims/forms.py
@@ -1,1 +1,32 @@
+from django import forms
 from .models import Post, Comment
+
+class PostForm(forms.ModelForm):
+    once_datetime = forms.DateTimeField(
+        widget = forms.DateTimeInput(
+            attrs = {
+                'type': 'datetime-local',
+            }
+        ),
+        required = False,
+    )
+    class Meta:
+        model = Post
+        fields = [
+            "title",
+            "content",
+            "category",
+            "many_datetime",
+            "once_datetime",
+            "limit",
+            "detail_address",
+            "price",
+            "image_first",
+        ]
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = [
+            "content"
+        ]
