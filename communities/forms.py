768858d9aed3944ae34338e0e7e2f8bd67dc161c
“SSONGNOSS66” <ssongnoss66@gmail.com>
2023-06-06 15:12:29 +0900

    UPDATE commu_BE

--- communities/forms.py
+++ communities/forms.py
@@ -5,28 +5,7 @@ from django.db.models import Count
 
 
 class PostForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-        # Get the available category choices from the database
-        category_choices = list(Post.objects.exclude(category__isnull=True).exclude(category='').values_list('category', 'category'))
-        
-        # Get the popular tags and add their names to the category_choices list
-        popular_tags = Tag.objects.annotate(num_times=Count('taggit_taggeditem_items')).filter(num_times__gt=5)
-        for tag in popular_tags:
-            category_choices.append((tag.name, tag.name))
-        
-        # Add a blank option at the beginning of the category_choices list
-        category_choices.insert(0, ('', ''))
-        arr = []
-        for val in category_choices:
-            if val not in arr:
-                arr.append(val)
-        
-        category_choices = arr
-        
-        self.fields['category'] = forms.ChoiceField(choices=category_choices, required=False)
-
+    category = forms.ChoiceField(label='카테고리', label_suffix='', choices=Post.category_Choices)
     class Meta:
         model = Post
         fields = [
