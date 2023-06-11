3f90dcae4e2a38dabc474d8f8eaf443f6e55d445
“SSONGNOSS66” <ssongnoss66@gmail.com>
2023-06-11 16:28:23 +0900

    UPDATE sns_share

--- communities/views.py
+++ communities/views.py
@@ -6,6 +6,11 @@ from django.http import JsonResponse
 from django.db.models import Q, Count
 from taggit.models import Tag
 from datetime import datetime, timedelta
+import os
+from dotenv import load_dotenv
+load_dotenv()
+KAKAO_JS_KEY = os.getenv('KAKAO_JS_KEY')
+KAKAO_API_KEY = os.getenv('KAKAO_API_KEY')
 
 # Create your views here.
 def index(request):
@@ -98,7 +103,8 @@ def detail(request, post_pk):
         'comment_form': comment_form,
         'comment': comment,
         'comment_section_id': comment_section_id,
-        'likes_count': post.like_users.count()-post.dislike_users.count()
+        'likes_count': post.like_users.count()-post.dislike_users.count(),
+        'KAKAO_JS_KEY': KAKAO_JS_KEY,
     }
     response = render(request, 'communities/detail.html', context)
 
